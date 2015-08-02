a44fea18dfa7595ae7288c8399c3e45b1c290bc8
Harriet Craven <harrietc52@hotmail.co.uk>
2015-08-02 16:00:55 +0100

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Harriet Craven (harrietc52@hotmail.co.uk)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
